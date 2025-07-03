16f2189c41c0dc4748224469463c0508e3deb0c4
naibhivenkat <venkat.naibhi@gmail.com>
2025-07-03 20:58:15 +0530

    test build

--- app/src/main/java/com/example/randomguess/MainActivity.java
+++ app/src/main/java/com/example/randomguess/MainActivity.java
@@ -1,29 +1,19 @@
 package com.example.randomguess;
 
 import android.os.Bundle;
-import android.widget.*;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import com.chaquo.python.*;
 
 public class MainActivity extends AppCompatActivity {
-    int number = (int) (Math.random() * 100) + 1;
+    static {
+        System.loadLibrary("python3.10");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Python.start(new AndroidPlatform(this));
-        setContentView(R.layout.activity_main);
-
-        EditText input = findViewById(R.id.input);
-        Button checkBtn = findViewById(R.id.checkBtn);
-        TextView result = findViewById(R.id.result);
-
-        checkBtn.setOnClickListener(v -> {
-            String guess = input.getText().toString();
-            PyObject res = Python.getInstance()
-                                 .getModule("game")
-                                 .callAttr("check_guess", guess, number);
-            result.setText(res.toString());
-        });
+        TextView textView = new TextView(this);
+        textView.setText("Welcome to Random Number Game!");
+        setContentView(textView);
     }
 }
